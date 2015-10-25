5a55b7c31ddce0b3f9c14517b2d10fb954edbf7d
Jon Jaques <jon@3five.com>
2015-10-25 15:52:05 -0500

    Tweak app startup

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ require('babel/register')({ stage: 0 })
 var app, port, http = require('http')
 
 global.__SERVER__ = true;
+global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production'
 
-if (require("piping")()) {
-  port = process.env.PORT || 3000
-  app = require('./src/server/main')
-  http.createServer(app).listen(port)
-  console.log('Listening on ' + port)
+if (__DEVELOPMENT__) {
+  if (!require('piping')()) {
+    return;
+  }
 }
+
+port = process.env.PORT || 3000
+app = require('./src/server/main')
+http.createServer(app).listen(port)
+console.log('Listening on ' + port)
